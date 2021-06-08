51a602597aac0b5aef0fc1a6cb136d87c50fe498
Robel Milashu <robelwo@gmail.com>
2021-06-08 15:08:26 +0300

    home page

---
+++ lib/screens/Home.dart
@@ -0,0 +1,87 @@
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _controller = TextEditingController();
+  var _channel = WebSocketChannel.connect(
+    Uri.parse('ws://192.168.43.30:8100/TobiaChat/endpoint'),
+  );
+  @override
+  void initState() {
+    super.initState();
+    _channel = WebSocketChannel.connect(
+      Uri.parse('ws://192.168.43.30:8100/TobiaChat/endpoint'),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey[50],
+        foregroundColor: Colors.black,
+        iconTheme: IconThemeData(color: Colors.grey),
+        title: Text(
+          "Tobia Chat",
+          style: TextStyle(
+              fontSize: 22, fontWeight: FontWeight.bold, color: Colors.grey),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            ListTile(),
+            Form(
+              child: TextFormField(
+                controller: _controller,
+                decoration: InputDecoration(labelText: 'Send a message'),
+              ),
+            ),
+            SizedBox(height: 24),
+            StreamBuilder(
+              stream: _channel.stream,
+              builder: (context, snapshot) {
+                return Text(snapshot.hasData ? '${snapshot.data}' : '');
+              },
+            ),
+            OutlinedButton(onPressed: _sendMessage, child: Text('Send'))
+          ],
+        ),
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.grey[50],
+          onPressed: _sendMessage,
+          tooltip: 'Send message',
+          child: Center(
+            child: Text(
+              '.',
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold),
+            ),
+          )), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void _sendMessage() {
+    if (_controller.text.isNotEmpty) {
+      _channel.sink.add(_controller.text);
+    }
+  }
+
+  @override
+  void dispose() {
+    _channel.sink.close();
+    super.dispose();
+  }
+}
