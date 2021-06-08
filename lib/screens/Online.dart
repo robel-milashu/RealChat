10572363b346452d754d2998df70a8e2e361976f
Robel Milashu <robelwo@gmail.com>
2021-06-08 15:07:27 +0300

    online page update

--- lib/screens/Online.dart
+++ lib/screens/Online.dart
@@ -9,8 +9,23 @@ class Online extends StatefulWidget {
 class _OnlineState extends State<Online> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(),
+    return ListView.builder(
+        itemCount: 15,
+        itemBuilder: (context, index) {
+          return list();
+        });
+  }
+
+  Widget list() {
+    return ListTile(
+      leading: CircleAvatar(
+        child: Text("A"),
+      ),
+      title: Text("Abebe"),
+      trailing: Text(
+        '.',
+        style: TextStyle(fontSize: 30),
+      ),
     );
   }
 }
